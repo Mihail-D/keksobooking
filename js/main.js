eebbf8d5fddf7c9bfd846cdf8fa5bd75cb7e40b9
Mihail-D <nuby.4register@gmail.com>
2019-07-01 14:18:17 +0300

    +

--- js/main.js
+++ js/main.js
@@ -19,14 +19,14 @@ var mapWidth = document.querySelector('.map__pins').offsetWidth;
 var getOffers = function (index) {
   return {
     author: {
-      avatar: 'img/avatars/user0' + (index + 1) + '.png'
+      avatar: 'img/avatars/user0' + (index + 1) + '.png',
     },
     offer: {
-      type: OFFERS_TYPES[getRandomNumber(0, OFFERS_TYPES.length)]
+      type: OFFERS_TYPES[getRandomNumber(0, OFFERS_TYPES.length)],
     },
     location: {
       x: getRandomNumber(0, mapWidth),
-      y: getRandomNumber(MIN_Y, MAX_Y)
+      y: getRandomNumber(MIN_Y, MAX_Y),
     },
   };
 };
@@ -47,7 +47,7 @@ var createPin = function (pin) {
   var pinElement = pinTemplate.cloneNode(true);
   pinElement.style.left = pin.location.x + PIN_OFFSET_X + 'px';
   pinElement.style.top = pin.location.y + PIN_OFFSET_Y + 'px';
-  
+
   var avatar = pinElement.querySelector('img');
   avatar.src = pin.author.avatar;
   avatar.alt = 'Personal Avatar';
