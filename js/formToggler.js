b80ccbe3a557ba28470484e3a6b2af12597aee11
Mihail-D <nuby.4register@gmail.com>
2019-07-26 21:43:25 +0300

    +

--- js/formToggler.js
+++ js/formToggler.js
@@ -1,87 +1,87 @@
 'use strict';
+(function () {
+  window.MIN_Y = 130;
+  window.PIN_OFFSET_X = -61;
+  window.MAX_Y = 630;
+  window.formMain = document.querySelector('.ad-form');
+  window.mapWidth = document.querySelector('.map__pins').offsetWidth;
+  window.mapBlock = document.querySelector('.map');
+  window.pinFragment = document.createDocumentFragment();
 
-window.MIN_Y = 130;
-window.PIN_OFFSET_X = -61;
-window.MAX_Y = 630;
-window.formMain = document.querySelector('.ad-form');
-window.mapWidth = document.querySelector('.map__pins').offsetWidth;
-window.mapBlock = document.querySelector('.map');
-window.pinFragment = document.createDocumentFragment();
+  var enableForm = function () {
+    window.formMain.classList.remove('ad-form--disabled');
+  };
 
-var enableForm = function () {
-  window.formMain.classList.remove('ad-form--disabled');
-};
+  var enableMap = function () {
+    window.mapBlock.classList.remove('map--faded');
+  };
 
-var enableMap = function () {
-  window.mapBlock.classList.remove('map--faded');
-};
+  var enableInput = function () {
+    var inputFields = window.formMain.querySelectorAll('input');
+    for (var j = 0; j < inputFields.length; j++) {
+      inputFields[j].disabled = false;
+    }
+  };
 
-var enableInput = function () {
-  var inputFields = window.formMain.querySelectorAll('input');
-  for (var j = 0; j < inputFields.length; j++) {
-    inputFields[j].disabled = false;
-  }
-};
+  var enableSelect = function () {
+    var inputSelect = window.formMain.querySelectorAll('select');
+    for (var k = 0; k < inputSelect.length; k++) {
+      inputSelect[k].disabled = false;
+    }
+  };
 
-var enableSelect = function () {
-  var inputSelect = window.formMain.querySelectorAll('select');
-  for (var k = 0; k < inputSelect.length; k++) {
-    inputSelect[k].disabled = false;
-  }
-};
+  var enableTextField = function () {
+    var textField = window.formMain.querySelector('textarea');
+    textField.disabled = false;
+  };
 
-var enableTextField = function () {
-  var textField = window.formMain.querySelector('textarea');
-  textField.disabled = false;
-};
+  var enableButton = function () {
+    var button = window.formMain.querySelectorAll('button');
+    for (var l = 0; l < button.length; l++) {
+      button[l].disabled = false;
+    }
+  };
 
-var enableButton = function () {
-  var button = window.formMain.querySelectorAll('button');
-  for (var l = 0; l < button.length; l++) {
-    button[l].disabled = false;
-  }
-};
+  window.pinMain = document.querySelector('.map__pin--main');
 
-window.pinMain = document.querySelector('.map__pin--main');
+  window.getPageElements = function () {
+    enableMap();
+    enableForm();
+    enableInput();
+    enableSelect();
+    enableTextField();
+    enableButton();
 
+    var addressField = window.formMain.querySelector('#address');
 
-window.getPageElements = function () {
-  enableMap();
-  enableForm();
-  enableInput();
-  enableSelect();
-  enableTextField();
-  enableButton();
+    var mapRect = window.mapBlock.getBoundingClientRect();
+    var pinRect = window.pinMain.getBoundingClientRect();
+    addressField.value = pinRect.left - mapRect.left + ', ' + (pinRect.top - mapRect.top);
 
-  var addressField = window.formMain.querySelector('#address');
+    window.mapBlock.appendChild(window.pinFragment);
+  };
 
-  var mapRect = window.mapBlock.getBoundingClientRect();
-  var pinRect = window.pinMain.getBoundingClientRect();
-  addressField.value = pinRect.left - mapRect.left + ', ' + (pinRect.top - mapRect.top);
+  var disableInputFields = function () {
+    var inputFields = window.formMain.querySelectorAll('input');
+    for (var x = 0; x < inputFields.length; x++) {
+      inputFields[x].disabled = true;
+    }
 
-  window.mapBlock.appendChild(window.pinFragment);
-};
+    var inputSelect = window.formMain.querySelectorAll('select');
+    for (var y = 0; y < inputSelect.length; y++) {
+      inputSelect[y].disabled = true;
+    }
 
-var disableInputFields = function () {
-  var inputFields = window.formMain.querySelectorAll('input');
-  for (var x = 0; x < inputFields.length; x++) {
-    inputFields[x].disabled = true;
-  }
+    var textField = window.formMain.querySelector('textarea');
+    textField.disabled = true;
 
-  var inputSelect = window.formMain.querySelectorAll('select');
-  for (var y = 0; y < inputSelect.length; y++) {
-    inputSelect[y].disabled = true;
-  }
+    var buttonsBlock = window.formMain.querySelector('.ad-form__element--submit');
 
-  var textField = window.formMain.querySelector('textarea');
-  textField.disabled = true;
+    var button = buttonsBlock.querySelectorAll('button');
+    for (var z = 0; z < button.length; z++) {
+      button[z].disabled = true;
+    }
+  };
 
-  var buttonsBlock = window.formMain.querySelector('.ad-form__element--submit');
-
-  var button = buttonsBlock.querySelectorAll('button');
-  for (var z = 0; z < button.length; z++) {
-    button[z].disabled = true;
-  }
-};
-
-disableInputFields();
+  disableInputFields();
+})();
